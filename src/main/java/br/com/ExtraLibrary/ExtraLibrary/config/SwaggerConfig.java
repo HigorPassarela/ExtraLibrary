80f6680814d88f3b112ff26ae6cd508a9b28cd5d
HigorPassarela <hhpassarela@gmail.com>
2026-03-06 14:31:35 -0300

    fix: SwaggerConfig cors

--- src/main/java/br/com/ExtraLibrary/ExtraLibrary/config/SwaggerConfig.java
+++ src/main/java/br/com/ExtraLibrary/ExtraLibrary/config/SwaggerConfig.java
@@ -19,9 +19,6 @@ public class SwaggerConfig {
 
     private final Environment environment;
 
-    @Value("${server.port}")
-    private String serverPort;
-
     public SwaggerConfig(Environment environment) {
         this.environment = environment;
     }
@@ -77,10 +74,7 @@ public class SwaggerConfig {
                 return Arrays.asList(
                         new Server()
                                 .url("https://extralibrary-hom.up.railway.app")
-                                .description("🧪 Homolog Server"),
-                        new Server()
-                                .url("https://extralibrary-dev.up.railway.app")
-                                .description("🔧 Development Server")
+                                .description("🧪 Homolog Server")
                 );
 
             case "development":
@@ -88,10 +82,7 @@ public class SwaggerConfig {
                 return Arrays.asList(
                         new Server()
                                 .url("https://extralibrary-dev.up.railway.app")
-                                .description("🔧 Development Server"),
-                        new Server()
-                                .url("http://localhost:" + serverPort)
-                                .description("💻 Local Development")
+                                .description("🔧 Development Server")
                 );
         }
     }
